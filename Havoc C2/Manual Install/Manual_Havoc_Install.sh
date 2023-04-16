1a4c16bd66378bf3488cca9c7f6c7e5bb382d069
emancipat3r <ande2541@gmail.com>
2023-04-15 23:03:06 -0400

    p

--- Havoc C2/Manual Install/Manual_Havoc_Install.sh
+++ Havoc C2/Manual Install/Manual_Havoc_Install.sh
@@ -5,43 +5,44 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-echo -e "\n\n\nStep 1: ----> BUILDING THE CLIENT"
+echo -e "\n======================================="
+echo "#        BUILDING CLIENT              #"
+echo "======================================="
 
 echo -e "\n\n\nStep 1.1: Downgrading libvulkan1"
 apt-get install -y --allow-downgrades libvulkan1=1.2.162.0-1 libhwloc15=2.4.1+dfsg-1 libnss3=2:3.61-1+deb11u3 libnspr4-dev=2:4.29-1 libnspr4=2:4.29-1
 
-#echo -e "\n\n\nStep 1.2: Installing aptitude"
-#apt-get install -y aptitude
-
-echo -e "\n\n\nStep 1.3: Installing Havoc C2 & Python 3.10 requirements"
+echo -e "\n\n\nStep 1.2: Installing Havoc C2 & Python 3.10 requirements"
 apt-get install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm zlib1g-dev libnss3-dev wget libvulkan-dev
 
-echo -e "\n\n\nStep 1.4: Downloading Python 3.10"
+echo -e "\n\n\nStep 1.3: Downloading Python 3.10"
 wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 
-echo -e "\n\n\nStep 1.5: Extracting Python 3.10 and moving into the extracted directory"
+echo -e "\n\n\nStep 1.4: Extracting Python 3.10 and moving into the extracted directory"
 tar -xvf Python-3.10.0.tgz
 cd Python-3.10.0
 
-echo -e "\n\n\nStep 1.6: Configuring and compiling Python 3.10 source code"
+echo -e "\n\n\nStep 1.5: Configuring and compiling Python 3.10 source code"
 ./configure --enable-optimizations
 make -j$(nproc)
 
-echo -e "\n\n\nStep 1.7: Installing the compiled Python 3.10"
+echo -e "\n\n\nStep 1.6: Installing the compiled Python 3.10"
 make altinstall
 
-echo -e "\n\n\nStep 1.8: Preparing for git clone"
+echo -e "\n\n\nStep 1.7: Preparing for git clone"
 cd /opt
 git clone https://github.com/HavocFramework/Havoc.git
 cd Havoc/Client
 
-echo -e "\n\n\nStep 1.9: Modifying /opt/Havoc/Client/CMakeLists.txt"
+echo -e "\n\n\nStep 1.8: Modifying /opt/Havoc/Client/CMakeLists.txt"
 sed -i '/${Boost_LIBRARIES}/a \        ${CMAKE_DL_LIBS}\n        util' CMakeLists.txt
 
-echo -e "\n\n\nStep 1.10: Compiling Havoc C2 Client"
+echo -e "\n\n\nStep 1.9: Compiling Havoc C2 Client"
 make
 
-echo -e "\n\n\nStep 2: ----> BUILDING THE TEAMSERVER"
+echo -e "\n======================================="
+echo "#        BUILDING TEAMSERVER          #"
+echo "======================================="
 
 echo -e "\n\n\nStep 2.1: Navigating into Teamserver directory"
 cd /opt/Havoc/Teamserver
